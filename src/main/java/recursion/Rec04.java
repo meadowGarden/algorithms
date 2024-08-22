34febf1a3c0ec7793a356e10e703b82bed93d71f
dariusLenovoPC <jomantasdarius@yahoo.com>
2024-08-22 10:56:17 +0300

    still on quick sort

--- src/main/java/recursion/Rec04.java
+++ src/main/java/recursion/Rec04.java
@@ -15,8 +15,55 @@ public class Rec04 {
         if (arr.length < 2) {
             return arr;
         } else {
-            int pivot = arr[0];
 
+            int pivot = arr[0];
+            int lessCount = 0;
+            int greaterCount = 0;
+
+            for (int n = 1; n < arr.length; n++) {
+                if (arr[n] <= pivot) {
+                    lessCount++;
+                } else {
+                    greaterCount++;
+                }
+            }
+
+            int[] lessArr = new int[lessCount];
+            int takenLessIndex = 0;
+            int[] greaterArr = new int[greaterCount];
+            int takenGreaterIndex = 0;
+
+            for (int n = 1; n < arr.length; n++) {
+                if (arr[n] <= pivot) {
+                    lessArr[takenLessIndex] = arr[n];
+                    takenLessIndex++;
+                }
+
+                if (arr[n] > pivot) {
+                    greaterArr[takenGreaterIndex] = arr[n];
+                    takenGreaterIndex++;
+                }
+            }
+
+            lessArr = quickSort(lessArr);
+            greaterArr = quickSort(greaterArr);
+
+            int addIndex = 0;
+            for (int i = addIndex; i < lessArr.length; i++) {
+                arr[i] = lessArr[addIndex];
+                addIndex++;
+            }
+            addIndex = (lessArr.length > 0) ? lessArr.length - 1 : lessArr.length;
+
+            arr[addIndex] = pivot;
+            addIndex++;
+
+            for (int i = addIndex; i < greaterArr.length; i++) {
+                arr[i] = greaterArr[addIndex];
+                addIndex++;
+            }
+
+            return arr;
         }
     }
 }
